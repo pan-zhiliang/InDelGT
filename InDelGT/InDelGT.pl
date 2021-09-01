6d1add94393d4744f75a1fe96ac44b04eab63ecc
pan-zhiliang <74814023+pan-zhiliang@users.noreply.github.com>
2021-09-01 20:32:26 +0800

    Update InDelGT.pl

--- InDelGT/InDelGT.pl
+++ InDelGT/InDelGT.pl
@@ -4,10 +4,10 @@ use Getopt::Long;
 use Parallel::ForkManager;
 
 sub prtHelp{
-	print "\nInDelGT is an integrated pipeline for extracting InDel genotypes in a hybrid population with next genera-tion DNA sequencing data.\n";
+	print "\nInDelGT is an integrated pipeline for extracting InDel genotypes in a hybrid population with next generation DNA sequencing data.\n";
 	print "Contact: Chunfa Tong <tongchf\@njfu.edu.cn>\n";
 	print "Version: 1.0\n";
-	print "Usage: perl InDelGT.pl [option] <type> -o directory\n";
+	print "Usage: perl InDelGT.pl [option] <type> -o <directory>\n";
 	print "\n";
 	print "Options:\n";
 	print "	-p  <type>      the type of population: CP;BC1;BC2;F2 (default: CP)\n";
