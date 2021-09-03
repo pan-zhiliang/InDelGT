ac20867be95f74d147216dd870bd56cda3f98bc7
pan-zhiliang <74814023+pan-zhiliang@users.noreply.github.com>
2021-09-03 10:12:43 +0800

    Update InDelGT.pl

--- InDelGT/InDelGT.pl
+++ InDelGT/InDelGT.pl
@@ -149,7 +149,7 @@ my $parent1_fq_file1="$datafold/$parent1_fq[0]";
 my $parent1_fq_file2="$datafold/$parent1_fq[1]";
 my $parent2_fq_file1="$datafold/$parent2_fq[0]";
 my $parent2_fq_file2="$datafold/$parent2_fq[1]";
-if($outDirName=~/\/$/ and ($outDirName!~/\./ or $outDirName!~/\.\./)){
+if($outDirName=~/\/$/){
 	$outDirName=~s/\/$//g;
 }
 if($outDirName=~/^\./ or $outDirName=~/^\.\./ or $outDirName!~/\//){
